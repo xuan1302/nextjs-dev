fcad9aa5dd36b79bab0d6614961b589a380b9540
suan <xuaan.ndx@gmail.com>
2022-08-01 00:02:26 +0700

    update dynamic, shalldow router

---
+++ component/Header.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface HeaderProps {
+}
+
+export default function Header (props: HeaderProps) {
+    console.log('render-header')
+  return (
+    <div className='header'>
+      Header
+    </div>
+  );
+}
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,44 @@
-import  React from 'react';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import  React, { useEffect, useState } from 'react';
+// import Header from '../component/Header';
 
+const Header = dynamic(()=>import ('../component/Header'),{ssr:false})
 export interface AboutPageProps {
 }
 
 export default function AboutPage (props: AboutPageProps) {
+  const [postList,setPostList] = useState([])
+  const router = useRouter()
+  const page = Number(router.query?.page) || 1;
+
+  useEffect(()=>{
+    if(!page) return;
+    (
+      async()=>{
+        const repons = await fetch(`https:js-post-api.herokuapp.com/api/posts?_page=${page}`)
+        const data = await repons.json()
+        setPostList(data.data)
+      }
+    )();
+  },[page])
+  const handleClickNextPage = ()=>{
+    router.push({
+      pathname:'/about',
+      query:{
+        page:Number( router.query.page || 1) + 1
+      }
+    },undefined,
+    {shallow:true}) //khong chay trong sever
+  }
   return (
     <div>
-      About Page
+      <h1>About Page</h1>
+      <Header />
+      <ul className='post-list'>
+        {postList.map((post:any)=>(<li key={post.id}>{post.title}</li>))}
+      </ul>
+      <button onClick={handleClickNextPage}>Next page</button>
     </div>
   );
 }
